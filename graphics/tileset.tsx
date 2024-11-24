8618afb8becb44a12774670eaf1b10449e63a284
fwyr <105589546+fwyr@users.noreply.github.com>
2024-11-24 11:45:20 +0800

    change tileset location

---
+++ graphics/tileset.tsx
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="amongus" tilewidth="64" tileheight="64" tilecount="39" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="house/IMG_0629.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="1">
+  <image source="house/IMG_0630.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="house/IMG_0635.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="house/IMG_0636.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="house/IMG_0638.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="house/IMG_0637.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="8">
+  <image source="house/IMG_0640.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="9">
+  <image source="house/IMG_0641.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="10">
+  <image source="house/IMG_0642.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="11">
+  <image source="house/IMG_0644.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="13">
+  <image source="house/house tiles/IMG_0647.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="14">
+  <image source="house/house tiles/IMG_0648.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="15">
+  <image source="house/house tiles/IMG_0649.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="16">
+  <image source="house/house tiles/IMG_0650.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="17">
+  <image source="house/house tiles/IMG_0651.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="18">
+  <image source="house/house tiles/IMG_0652.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="19">
+  <image source="house/house tiles/IMG_0654.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="20">
+  <image source="house/house tiles/IMG_0656.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="21">
+  <image source="house/house tiles/IMG_0657.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="22">
+  <image source="house/house tiles/IMG_0658.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="23">
+  <image source="house/house tiles/IMG_0659.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="24">
+  <image source="house/house tiles/IMG_0660.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="25">
+  <image source="house/house tiles/IMG_0665.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="32">
+  <image source="house/house tiles/IMG_0665.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="33">
+  <image source="house/house tiles/IMG_0666.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="34">
+  <image source="house/house tiles/IMG_0667.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="35">
+  <image source="house/house tiles/IMG_0668.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="36">
+  <image source="house/house tiles/IMG_0672.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="37">
+  <image source="house/house tiles/IMG_0673.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="38">
+  <image source="house/house tiles/IMG_0674.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="39">
+  <image source="house/house tiles/IMG_0677.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="40">
+  <image source="house/house tiles/IMG_0678.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="41">
+  <image source="house/house tiles/IMG_0679.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="42">
+  <image source="house/house tiles/IMG_0680.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="43">
+  <image source="house/house tiles/IMG_0681.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="44">
+  <image source="house/house tiles/IMG_0682.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="45">
+  <image source="house/house tiles/IMG_0683.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="46">
+  <image source="house/house tiles/IMG_0684.PNG" width="64" height="64"/>
+ </tile>
+ <tile id="47">
+  <image source="house/house tiles/new sofa /IMG_0663.PNG" width="64" height="64"/>
+ </tile>
+</tileset>
